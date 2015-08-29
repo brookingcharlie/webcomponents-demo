4858ab3e7164c932929139c8ad72a9d90856862a
Charles Brooking <charlie@brooking.id.au>
2015-08-29 16:25:34 +1000

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Custom Elements example
+
+Simple example showing the definition, registration, and usage of a Custom Element.
+
+As of August 2015, this example is only supported by Chrome/Chromium.
+For support in other browsers, look at Polymer's
+[web components polyfills](https://www.polymer-project.org/docs/start/platform.html).
+
+References:
+
+* http://w3c.github.io/webcomponents/spec/custom/
+* http://www.html5rocks.com/en/tutorials/webcomponents/customelements/
---
+++ hello-world.js
@@ -0,0 +1,26 @@
+var HelloWorld = document.registerElement("hello-world", {
+  prototype: Object.create(HTMLElement.prototype, {
+    createdCallback: {
+      value: function() {
+        var message = this.getAttribute("message");
+
+        var headingElement = document.createElement("h1");
+        headingElement.appendChild(new Text("Hello"));
+
+        this.messageElement = document.createElement("p");
+        this.messageElement.className = "message";
+        this.messageElement.textContent = message;
+ 
+        this.appendChild(headingElement);
+        this.appendChild(this.messageElement);
+      }
+    },
+    attributeChangedCallback: {
+      value: function(attrName, oldVal, newVal) {
+        if (attrName === "message") {
+          this.messageElement.textContent = newVal;
+        }
+      }
+    }
+  })
+});
